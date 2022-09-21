57da43498dc0ac035a8d57665c86cc850ed90bb5
Iddriss Raaj <idd.iraaj@gmail.com>
2022-09-21 01:48:57 +0000

    add buildlist

---
+++ src/buildList.js
@@ -0,0 +1,28 @@
+const { version } = require("../package.json");
+const mainnet = require("./tokens/mainnet.json");
+const goerli = require("./tokens/goerli.json");
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  const list = {
+    name: "StarkDefi Default",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "ipfs://QmYVm6z2TmQYzhG1rV6jHi8nfE9a5HSFAy9vSE1ruBvRZd",
+    keywords: ["starkdefi", "default"],
+    tokens: [...mainnet, ...goerli]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+  return list;
+};
